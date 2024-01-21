60333248534ac0ca9fe87082c79bc09c01a0ecd2
rvishakhs <rvishakhs@gmail.com>
2024-01-21 22:45:16 +0000

    fhgvjbk

--- CrudApplication/Program.cs
+++ CrudApplication/Program.cs
@@ -2,6 +2,9 @@ using Entities;
 using ServiceContracts;
 using Services;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
@@ -9,10 +12,10 @@ builder.Services.AddControllersWithViews();
 // Add services to the container.
 builder.Services.AddSingleton<ICountriesService, CountriesServices>();
 builder.Services.AddSingleton<IPersonService, PersonServices>();
-builder.Services.AddDbContext<PersonsDbContext>(options =>
-{
-    options.UseSqlServer();
-});
+// builder.Services.AddDbContext<PersonsDbContext>(options =>
+// {
+//     options.UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=admin;Database=personDatabase");
+// });
 
 var app = builder.Build();
 
@@ -21,6 +24,26 @@ if (builder.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
+
+    static void Main()
+    {
+        string connectionString = "Host=localhost;Port=5432;Username=postgres;Password=admin;Database=personDatabase";
+
+        using (var connection = new NpgsqlConnection(connectionString))
+        {
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Connected to PostgreSQL!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+
+Main();
 
 app.UseStaticFiles();
 app.UseRouting();
--- Entities/PersonsDbContext.cs
+++ Entities/PersonsDbContext.cs
@@ -4,14 +4,42 @@ namespace Entities;
 
 public class PersonsDbContext : DbContext
 {
+
+     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+     {
+             optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=admin;Database=personDatabase"); //Options
+     }
+     
+          
      public DbSet<Country> Countries { get; set; }
      public DbSet<Person> Persons { get; set; }
-
      protected override void OnModelCreating(ModelBuilder modelBuilder)
      {
           base.OnModelCreating(modelBuilder);
           
-          modelBuilder.Entity<Country>().ToTable("Countries");
+          modelBuilder.Entity<Country>().ToTable("Countries"); 
           modelBuilder.Entity<Person>().ToTable("Persons");
+          
+          //seed countries data
+
+          string countriesJson = System.IO.File.ReadAllText("countries.json");
+          List<Country> countries = System.Text.Json.JsonSerializer.Deserialize<List<Country>>(countriesJson);
+
+          foreach (Country country in countries)
+          {
+               modelBuilder.Entity<Country>().HasData(country);
+          }
+          
+          //seed persons data
+          string personsJson = System.IO.File.ReadAllText("persons.json");
+          List<Person> persons = System.Text.Json.JsonSerializer.Deserialize<List<Person>>(personsJson);
+
+          foreach (Person person in persons)
+          {
+               modelBuilder.Entity<Person>().HasData(persons);
+          }
+          
      } 
+     
+
 } 
